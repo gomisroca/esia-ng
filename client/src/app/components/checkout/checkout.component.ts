44638ea7d29c87ae7568ac043673b96a9b16b838
asfalfa <glkin9@gmail.com>
2024-03-12 20:31:11 +0100

    fix(FE): :fire: Removed unused refresh fn from checkout

--- client/src/app/components/checkout/checkout.component.ts
+++ client/src/app/components/checkout/checkout.component.ts
@@ -7,35 +7,26 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit{
-  public cart : any = localStorage.getItem('ESIA_Cart');
+    public cart : any = localStorage.getItem('ESIA_Cart');
 
-  constructor(private _snackBar: MatSnackBar) { }
+    constructor(private _snackBar: MatSnackBar) { }
 
-  ngOnInit(): void {
-    if(this.cart == null){
-      this.cart = [];
-    } else{
-      this.cart = JSON.parse(this.cart);
+    ngOnInit(): void {
+        if(this.cart == null){
+            this.cart = [];
+        } else{
+            this.cart = JSON.parse(this.cart);
+        }
+        console.log(this.cart)
     }
-    console.log(this.cart)
-  }
-  
-  refresh(): void {
-    this.cart = localStorage.getItem('ESIA_Cart');
-    if(this.cart == null){
-      this.cart = [];
-    } else{
-      this.cart = JSON.parse(this.cart);
-    }
-  }
 
-  removeFromCart(item: any): void {
-    let itemToRemove = this.cart.find((x: { id: any; }) => x.id == item.id);
-    const index = this.cart.indexOf(itemToRemove);
-    if (index > -1) {
-        this.cart.splice(index, 1);
-        this._snackBar.open(`Removed ${item.title} from your cart`, 'X', {duration: 3000});
+    removeFromCart(item: any): void {
+        let itemToRemove = this.cart.find((x: { id: any; }) => x.id == item.id);
+        const index = this.cart.indexOf(itemToRemove);
+        if (index > -1) {
+            this.cart.splice(index, 1);
+            this._snackBar.open(`Removed ${item.title} from your cart`, 'X', {duration: 3000});
+        }
+        localStorage.setItem('ESIA_Cart', JSON.stringify(this.cart));
     }
-    localStorage.setItem('ESIA_Cart', JSON.stringify(this.cart));
-  }
 }
