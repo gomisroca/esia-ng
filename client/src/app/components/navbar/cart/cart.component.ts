c1dc56f204d07179f22e52b73e91a38577d894c4
asfalfa <glkin9@gmail.com>
2024-03-12 20:31:34 +0100

    fix(FE): :bug: Fixed cart menu closing after removing item

--- client/src/app/components/navbar/cart/cart.component.ts
+++ client/src/app/components/navbar/cart/cart.component.ts
@@ -11,31 +11,32 @@ export class CartComponent implements OnInit{
 
   constructor(private _snackBar: MatSnackBar) { }
 
-  ngOnInit(): void {
-    if(this.cart == null){
-      this.cart = [];
-    } else{
-      this.cart = JSON.parse(this.cart);
+    ngOnInit(): void {
+        if(this.cart == null){
+            this.cart = [];
+        } else{
+            this.cart = JSON.parse(this.cart);
+        }
+        console.log(this.cart)
     }
-    console.log(this.cart)
-  }
   
-  refresh(): void {
-    this.cart = localStorage.getItem('ESIA_Cart');
-    if(this.cart == null){
-      this.cart = [];
-    } else{
-      this.cart = JSON.parse(this.cart);
+    refresh(): void {
+        this.cart = localStorage.getItem('ESIA_Cart');
+        if(this.cart == null){
+            this.cart = [];
+        } else{
+            this.cart = JSON.parse(this.cart);
+        }
     }
-  }
 
-  removeFromCart(item: any): void {
-    let itemToRemove = this.cart.find((x: { id: any; }) => x.id == item.id);
-    const index = this.cart.indexOf(itemToRemove);
-    if (index > -1) {
-        this.cart.splice(index, 1);
-        this._snackBar.open(`Removed ${item.title} from your cart`, 'X', {duration: 3000});
+    removeFromCart($event: any, item: any): void {
+        $event.stopPropagation();
+        let itemToRemove = this.cart.find((x: { id: any; }) => x.id == item.id);
+        const index = this.cart.indexOf(itemToRemove);
+        if (index > -1) {
+            this.cart.splice(index, 1);
+            this._snackBar.open(`Removed ${item.title} from your cart`, 'X', {duration: 3000});
+        }
+        localStorage.setItem('ESIA_Cart', JSON.stringify(this.cart));
     }
-    localStorage.setItem('ESIA_Cart', JSON.stringify(this.cart));
-  }
 }
